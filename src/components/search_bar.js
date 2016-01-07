903db759fbdae8460f1ee1b1d87aab3964ddce77
Memona Madi <madi.memona@gmail.com>
2016-01-07 13:18:17 +0000

    added functionalities

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-
 // Class based component using es6 class
 // that extends to the React.Component functionality
 class SearchBar extends Component {
   // All js classes have a function called constructor
   // Constructor is called automatically whenever a new instance of the class is created
-  // Constructor is used to initialize things (variables, states...)
+  // Constructor is used to initialize states in class based component
   constructor(props) {
     super(props);
     // this.state initializes the state
     // this property records changes in the input
     this.state = { term: '' };
   }
+  //
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange ={event => this.setState({ term: event.target.value})} />
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+// Youtube API key
 const API_KEY = 'AIzaSyCgyP7sQ_idV1mCVuBEJiwc43Xn8cx8hT8';
 
-// arrow is the ES6 way to declare a function
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+// Arrow is the ES6 way to declare a function
+// App renders the SearchBar component
+class App extends Component {
+  // When App first boots up
+  // constructor runs right away
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    // and kicks off a YT search
+    // then the callback function is called with list of videos
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // When key and value are exactly the same (like { videos: videos })
+      // we can use the shortcut { videos } (es6 syntax)
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos= { this.state.videos } />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
